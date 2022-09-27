924960bb0e8c59250571a95377ed607b458091dd
cheatky <itsinil@gmail.com>
2022-09-27 22:45:57 +0900

    feat(editor): add ckeditor height options

--- pages/ckeditor.tsx
+++ pages/ckeditor.tsx
@@ -1,34 +1,38 @@
-import type {NextPage} from 'next'
-import styles from '../styles/Editor.module.css'
-import Editor from "../snippet/ckeditor/Editor";
-import {useState} from "react";
+import type { NextPage } from 'next';
+import styles from '../styles/Editor.module.css';
+import Editor from '../snippet/ckeditor/Editor';
+import { useState } from 'react';
 
 const Ckeditor: NextPage = () => {
 
-    const [text, setText] = useState<string>('초기 데이터 입니다.');
+	const [text, setText] = useState<string>('초기 데이터 입니다.');
 
-    return (
-        <div className={styles.container}>
-            <main className={styles.main}>
-                <h1 className={styles.title}>
-                    CK Editor Code Snippet
-                </h1>
+	return (
+		<div className={styles.container}>
+			<main className={styles.main}>
+				<h1 className={styles.title}>
+					CK Editor Code Snippet
+				</h1>
 
-                <p className={styles.description}>
-                    아래는 샘플 코드 입니다.
-                </p>
+				<p className={styles.description}>
+					아래는 샘플 코드 입니다.
+				</p>
 
-                {/* 적용 코드 */}
-                <Editor initText={text}
-                        onChange={(newText) => setText(newText)}
-                        uploadHandler={(file) => new Promise(resolve => {
-                            resolve({default: 'http://image'})
-                        })}
-                        fallback={<div>로딩중</div>}
-                />
-            </main>
-        </div>
-    )
-}
+				{/* 적용 코드 */}
+				<Editor initText={text}
+						onChange={(newText) => setText(newText)}
+						uploadHandler={(file) => new Promise(resolve => {
+							resolve({ default: 'http://image' });
+						})}
+						options={{
+							minHeight: '200px', // default: inline
+							maxHeight: '500px', // default: infinite
+						}}
+						fallback={<div>로딩중...</div>}
+				/>
+			</main>
+		</div>
+	);
+};
 
-export default Ckeditor
+export default Ckeditor;
--- snippet/ckeditor/Editor.tsx
+++ snippet/ckeditor/Editor.tsx
@@ -1,63 +1,96 @@
-import React, {useEffect, useRef, useState} from 'react';
-import imageUploadPlugin, {AsyncImageUploadPromise} from "./plugin/ImageUploadPlugin";
+import React, { useEffect, useRef, useState } from 'react';
+import imageUploadPlugin, { AsyncImageUploadPromise } from './plugin/ImageUploadPlugin';
 
 export interface CKEditorRef {
-    CKEditor: any;
-    ClassicEditor: any;
+	CKEditor: any;
+	ClassicEditor: any;
+}
+
+export interface EditorOptions {
+	minHeight?: string;
+	maxHeight?: string;
 }
 
 export interface EditorProp {
-    initText?: string; // 초기 값
-    onChange?: (event, text) => void; // 변경 이벤트
-    onBlur?: (event, text) => void; // blur 이벤트
-    onFocus?: (event, text) => void; // focus 이벤트
-    fallback?: React.ReactElement; // 컴포넌트 로딩 fallback
-    uploadHandler: (file: File) => Promise<AsyncImageUploadPromise>; // 에디터 내 이미지 업로드 시 처리 및 노출 handler
+	initText?: string; // 초기 값
+	onChange?: (event, text) => void; // 변경 이벤트
+	onBlur?: (event, text) => void; // blur 이벤트
+	onFocus?: (event, text) => void; // focus 이벤트
+	fallback?: React.ReactElement; // 컴포넌트 로딩 fallback
+	options?: EditorOptions;
+	uploadHandler: (file: File) => Promise<AsyncImageUploadPromise>; // 에디터 내 이미지 업로드 시 처리 및 노출 handler
 }
 
-const Editor: React.FC<EditorProp> = ({initText, onChange, onFocus, onBlur, uploadHandler, fallback}) => {
-    const editorRef = useRef<CKEditorRef>();
-    const [isLayoutReady, setIsLayoutReady] = useState(false);
-    const [text, setText] = useState<string>(initText || '');
-    const {CKEditor, ClassicEditor} = editorRef.current || {};
+const Editor: React.FC<EditorProp> = ({
+										  initText,
+										  onChange,
+										  onFocus,
+										  onBlur,
+										  uploadHandler,
+										  fallback,
+										  options,
+									  }) => {
+	const editorRef = useRef<CKEditorRef>();
+	const [isLayoutReady, setIsLayoutReady] = useState(false);
+	const [text, setText] = useState<string>(initText || '');
+	const { CKEditor, ClassicEditor } = editorRef.current || {};
 
-    // 에디터 로드
-    useEffect(() => {
-        editorRef.current = {
-            CKEditor: require('@ckeditor/ckeditor5-react').CKEditor,
-            ClassicEditor: require('@ckeditor/ckeditor5-build-classic'),
-        };
-        setIsLayoutReady(true);
-    }, []);
+	// 에디터 로드
+	useEffect(() => {
+		editorRef.current = {
+			CKEditor: require('@ckeditor/ckeditor5-react').CKEditor,
+			ClassicEditor: require('@ckeditor/ckeditor5-build-classic'),
+		};
+		setIsLayoutReady(true);
+	}, []);
 
-    if (!isLayoutReady) {
-        return fallback ? fallback : <>Loading..</>
-    }
+	if (!isLayoutReady) {
+		return fallback ? fallback : <>Loading..</>;
+	}
 
-    return (
-        <>
-            <CKEditor
-                data={text}
-                config={{
-                    extraPlugins: [imageUploadPlugin({
-                        uploadHandler
-                    })],
-                }}
-                onChange={(event: any, editor: any) => {
-                    console.log(event, editor.getData())
-                    setText(editor.getData())
-                    onChange && onChange(event, editor.getData());
-                }}
-                onBlur={(event: any, editor: any) => {
-                    onBlur && onBlur(event, editor.getData());
-                }}
-                onFocus={(event: any, editor: any) => {
-                    onFocus && onFocus(event, editor.getData());
-                }}
-                editor={ClassicEditor}
-            />
-        </>
-    );
+	return (
+		<>
+			<CKEditor
+				data={text}
+				config={{
+					extraPlugins: [imageUploadPlugin({
+						uploadHandler,
+					})],
+				}}
+				onChange={(event: any, editor: any) => {
+					setText(editor.getData());
+					onChange && onChange(event, editor.getData());
+				}}
+				onBlur={(event: any, editor: any) => {
+					onBlur && onBlur(event, editor.getData());
+				}}
+				onFocus={(event: any, editor: any) => {
+					onFocus && onFocus(event, editor.getData());
+				}}
+				onReady={(editor) => {
+					// You can store the "editor" and use when it is needed.
+					// console.log("Editor is ready to use!", editor);
+					editor.editing.view.change((writer) => {
+						if(options?.maxHeight) {
+							writer.setStyle(
+								'max-height',
+								options?.maxHeight,
+								editor.editing.view.document.getRoot(),
+							);
+						}
+						if(options?.minHeight) {
+							writer.setStyle(
+								'min-height',
+								options?.minHeight,
+								editor.editing.view.document.getRoot(),
+							);
+						}
+					});
+				}}
+				editor={ClassicEditor}
+			/>
+		</>
+	);
 };
 
 
